f768a3b2e19b1ba8c04d57b9457b5c2fe2eaa2bc
s1030905 <s1030905@outlook.com>
2023-05-12 00:24:57 +0800

    create detail, edit, new, register page. And finish home routes

--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -2,10 +2,11 @@ const router = require('express').Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
+    const id = req.params.id
     const UserId = req.user.id
-    let todos = await Todo.findAll({ where: { UserId }, raw: true, nest: true })
+    let todos = await Todo.findByPk(id)
     res.render('index', { todos })
   } catch (error) {
     console.log(error)
