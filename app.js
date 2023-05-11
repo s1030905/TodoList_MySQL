f005f828cab87b48a8f93aba6d268eb7cacab190
s1030905 <s1030905@outlook.com>
2023-05-11 14:23:22 +0800

    feat: urlencoded and methodOverride

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const handlebars = require('express-handlebars')
+const methodOverride = require('method-override')
 const routes = require('./routes/index')
 
 
 app.set('view engine', 'handlebars')
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(routes)
 
 app.listen(PORT, () => {
